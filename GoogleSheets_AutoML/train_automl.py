0e83f7c1c6e657971dea8a634b5e014f2c864f16
shahules786 <shahules786@gmail.com>
2020-10-04 15:26:03 +0530

    train

--- GoogleSheets_AutoML/train_automl.py
+++ GoogleSheets_AutoML/train_automl.py
@@ -5,8 +5,10 @@ from sklearn.model_selection import train_test_split
 
 
 # get the training data
-df_name = "credit_card"
-cred_path = "/home/shahul/Downloads/mljar-1de77676a687.json"
+df_name = "sheet name"
+cred_path = "path/credentials.json"
+email = "shahules786@gmail.com"
+
 
 client = get_client(cred_path)
 
@@ -15,7 +17,7 @@ X_train, y_train = get_train_data(client,df_name)
 X_train,X_test,y_train,y_test = train_test_split(X_train,y_train,test_size=0.1)
 
 automl = AutoML(results_path="Automl_output",total_time_limit=10)
-automl.fit(X_train, y_train)
+#automl.fit(X_train, y_train)
 
 train_pred = automl.predict(X_train)
 test_pred = automl.predict(X_test)
@@ -23,4 +25,4 @@ test_pred = automl.predict(X_test)
 data = {'train_target':y_train,"train_prediction":train_pred,
         'test_target':y_test,'test_prediction':test_pred}
 
-write_out(client,data,"shahules786@gmail.com")
+write_out(client,data,email)
